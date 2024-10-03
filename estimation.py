2d1cb9765e7ff76312b1b8d22bba5ebc17daec04
commisoria <115154782+commisoria@users.noreply.github.com>
2024-10-03 15:40:19 +0300

    Update estimation.py

--- estimation.py
+++ estimation.py
@@ -4,9 +4,9 @@ from binance.client import Client
 from datetime import time,datetime
 import numpy as np
 import streamlit as st
-
 import requests
 
+#-------------------------------------------------------------------------IP--------------------------------
 def get_public_ip():
     response = requests.get('https://api.ipify.org?format=json')
     if response.status_code == 200:
@@ -17,6 +17,21 @@ def get_public_ip():
 # Get and print the public IP address
 public_ip = get_public_ip()
 st.write(public_ip)
+#--------------------------------------------------------------------------------------------------------------
+api_key='9WY9XfdvHWcCcN9Gn3ajwipyNNMK10DXU3HcodLxPyMbQJ5R4Gg1VbdApH0W9cZD'
+api_secret='bYWcoKb4iEz6GWRORww970rdsLsvWdEOGNFEJ2Zsr2CqrAqCf4ICEnKsqLhDnkV9'
+client=Client(api_key=api_key,api_secret=api_secret)
+# Get account information
+account_info = client.get_account()
+
+# Find the balance for PAXG
+for balance in account_info['balances']:
+    if balance['asset'] == 'PAXG':
+        st.write(balance['free'])
+
+
+
+
 
 
 
